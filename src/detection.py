a00c835331f181fb5360cf64173a7007e728f375
normanXCat <149684516+normanXCat@users.noreply.github.com>
2026-07-09 08:55:10 +0300

    style: nettoyer l'espacement et ajouter un saut de ligne final

--- src/detection.py
+++ src/detection.py
@@ -11,7 +11,6 @@ results = model.predict(source=0, classes=[0], conf=0.5, show=True)
 
 for result in results:
     boxes = result.boxes
-    
     for box in boxes:
         x1, y1, x2, y2 = box.xyxy[0]
         conf = box.conf[0]
